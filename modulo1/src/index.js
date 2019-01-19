bad7644fbcd21de25dd7922eab750a5715961463
ChilenoSika <jaum97@yahoo.com.br>
2019-01-19 02:49:54 -0200

    Adding state

--- modulo1/src/index.js
+++ modulo1/src/index.js
@@ -1,14 +1,28 @@
 import React, { Component } from "react";
-import { Platform, StyleSheet, Text, View } from "react-native";
+import { Platform, StyleSheet, Text, View, Button } from "react-native";
 
 import Todo from "./components/Todo";
 
 export default class App extends Component {
+  state = {
+    user: "banana cowboy",
+    todos: [{ id: 0, text: "fazer café" }, { id: 1, text: "estudar react" }]
+  };
+
+  addTodo = () => {
+    this.setState({
+      todos: [...this.state.todos, { id: Math.random(), text: "testy" }]
+    });
+  };
+
   render() {
     return (
       <View style={styles.container}>
-        <Todo title="fazer café" />
-        <Todo />
+        <Text>{this.state.user}</Text>
+        {this.state.todos.map(todo => (
+          <Todo key={todo.id} title={todo.text} />
+        ))}
+        <Button title="add todo" onPress={this.addTodo} />
       </View>
     );
   }
