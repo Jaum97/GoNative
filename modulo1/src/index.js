a60690f0b8a2bdbae65759a737f7963f8a4fa869
ChilenoSika <jaum97@yahoo.com.br>
2019-01-23 00:44:47 -0200

    reactotron

--- modulo1/src/index.js
+++ modulo1/src/index.js
@@ -1,6 +1,8 @@
 import React, { Component } from "react";
 import { StyleSheet, Text, View } from "react-native";
 
+import "./config/ReactotronConfig";
+
 export default class App extends Component {
   state = {
     user: "banana cowboy"
